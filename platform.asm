d1c42c171f88625f947144b1a4ae7a1d1f51d96b
Victor Goya <victor@mooveo.co>
2020-07-13 18:47:53 +0200

    add sprite

--- platform.asm
+++ platform.asm
@@ -5,14 +5,21 @@
 
 PLAYFIELD_WIDTH             = 4
 PLAYFIELD_HEIGHT            = 5
-TILE_HEIGHT                 = 7
+TILE_HEIGHT                 = 6
 PLAYFIELD_COLOR             = $a0
 LAVA_COLOR                  = $38
 BACKGROUND_COLOR            = $04
+PLAYER_ANIM_SPEED           = 128
 
     SEG.U vars
     ORG $80
 
+HAS_SPRITE             ds 0
+PLAYER_X               ds 1
+PLAYER_Y               ds 1
+PLAYER_Y_ADDR          ds 1
+PLAYER_ANIM_CTR        ds 1
+PLAYER_CHAR_FRAME      ds 2
 RANDOM                 ds 1
 PLAYFIELD              ds PLAYFIELD_WIDTH * PLAYFIELD_HEIGHT * 2
 
@@ -21,6 +28,11 @@ PLAYFIELD              ds PLAYFIELD_WIDTH * PLAYFIELD_HEIGHT * 2
 
 Reset
     CLEAN_START
+
+    lda #PLAYER_ANIM_SPEED
+    sta PLAYER_ANIM_CTR
+
+    SET_POINTER PLAYER_CHAR_FRAME, CharFrame0
 
 NextFrame
     VERTICAL_SYNC
@@ -37,18 +49,27 @@ GameKernel
     jsr UpdateRandom
     jsr GenerateGameKernelBackground
     jsr GenerateGameKernelBasePlayfield
+    jsr UpdateGameKernelPlayerPosition
+    jsr UpdateGameKernelPlayerAnim
 
     lda #%11111111
     sta PF0
     sta PF1
     sta PF2
 
+    lda PLAYER_Y
+    sta PLAYER_Y_ADDR
+
+    lda PLAYER_X
+    ldx #0
+    jsr FineAdjustSprite
+
     TIMER_WAIT
 
     TIMER_SETUP 192
 
     sta WSYNC
-    ; sta HMOVE
+    sta HMOVE
 
     lda #0
     sta VBLANK
@@ -63,27 +84,29 @@ TILE_Y SET 0
     REPEAT PLAYFIELD_HEIGHT
     SUBROUTINE
 
-    ldx #0
+    IF TILE_Y = PLAYFIELD_HEIGHT - 1
+        lda #%00010000
+        sta PF0
+    ENDIF ; Build entrance and exit
+
+    ldx #TILE_HEIGHT * 4
 .GameKernelLine
         sta WSYNC
-        IF TILE_Y = PLAYFIELD_HEIGHT - 1
-            lda #0
-            sta PF0
-        ENDIF ; Build entrance and exit
 
         ldy TileDivideTable,x
-    
+
         lda (PLAYFIELD + TILE_Y * PLAYFIELD_WIDTH * 2),y
         sta PF1
         
         lda (PLAYFIELD + TILE_Y * PLAYFIELD_WIDTH * 2 + 2),y
         sta PF2
 
-        IF TILE_Y = PLAYFIELD_HEIGHT - 1
-            SLEEP 15
-        ELSE
-            SLEEP 20
-        ENDIF
+        ldy PLAYER_Y_ADDR
+        lda (PLAYER_CHAR_FRAME),y
+        sta GRP0
+        inc PLAYER_Y_ADDR
+
+        ldy TileDivideTable,x
 
         lda (PLAYFIELD + TILE_Y * PLAYFIELD_WIDTH * 2 + 4),y
         sta PF2
@@ -91,8 +114,7 @@ TILE_Y SET 0
         lda (PLAYFIELD + TILE_Y * PLAYFIELD_WIDTH * 2 + 6),y
         sta PF1
 
-        inx
-        cpx #TILE_HEIGHT * 4
+        dex
         bne .GameKernelLine
 
 TILE_Y SET TILE_Y + 1
@@ -112,6 +134,24 @@ TILE_Y SET TILE_Y + 1
     jsr ClearGameKernelPlayfield
 
     TIMER_WAIT
+    rts
+
+UpdateGameKernelPlayerAnim
+    dec PLAYER_ANIM_CTR
+    lda PLAYER_ANIM_CTR
+    cmp #128
+    bne .NotFrame0
+
+    SET_POINTER PLAYER_CHAR_FRAME, CharFrame0
+    jmp .NotFrame1
+
+.NotFrame0
+    cmp #0
+    bne .NotFrame1
+    SET_POINTER PLAYER_CHAR_FRAME, CharFrame1
+
+.NotFrame1
+    rts
 
 GenerateGameKernelFloor
     lda #%11111111
@@ -154,7 +194,7 @@ GenerateGameKernelBasePlayfield
     sta COLUBK    
 
     lda #%00000001
-    sta CTRLPF ; enable mirroring, that's useful for PF0
+    sta CTRLPF ; enable mirroring, that will come handy for PF0
 
     lda #%11110000
     sta PF0 ; build walls on the left and right
@@ -196,6 +236,37 @@ ClearGameKernelPlayfield
     sta PF2
     rts
 
+UpdateGameKernelPlayerPosition
+	lda #%10000000
+	bit SWCHA
+	bne DoneMoveRight
+
+    inc PLAYER_X
+DoneMoveRight
+
+	lda #%01000000
+	bit SWCHA
+	bne DoneMoveLeft
+
+    dec PLAYER_X
+DoneMoveLeft
+
+	lda #%00100000
+	bit SWCHA
+	bne DoneMoveDown
+
+    dec PLAYER_Y
+DoneMoveDown
+
+	lda #%00010000
+	bit SWCHA
+	bne DoneMoveUp
+
+    inc PLAYER_Y
+DoneMoveUp
+
+    rts
+
 UpdateRandom
     lda RANDOM
     asl
@@ -207,7 +278,7 @@ UpdateRandom
     sta RANDOM
     rts
 
-FineAdjust
+FineAdjustSprite
     sta WSYNC
     sec                      ; 02     Set the carry flag so no borrow will be applied during the division.
 .Divideby15
@@ -219,11 +290,60 @@ FineAdjust
     sta RESP0,x              ; 21/ 26/31/36/41/46/51/56/61/66/71 - Set the rough position.
     rts
 
+    BOUNDARY $ff ;; Force a page boundary crossing so "lda CharFrame0,y" takes the right amount of time
+CharFrame0
+    REPEAT 112
+    .byte #%00000000
+    REPEND
+    .byte #%00111100
+    .byte #%11111110
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%01111010
+    .byte #%01111110
+    .byte #%00111100
+    .byte #%00011000
+    .byte #%00111100
+    .byte #%11111110
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%01111010
+    .byte #%01111110
+    .byte #%00111100
+    .byte #%00011000
+    REPEAT 112
+    .byte #%00000000
+    REPEND
+
+    BOUNDARY $ff ;; Force a page boundary crossing so "lda CharFrame0,y" takes the right amount of time
+CharFrame1
+    REPEAT 112
+    .byte #%00000000
+    REPEND
+    .byte #%00111100
+    .byte #%00111110
+    .byte #%01111111
+    .byte #%11111111
+    .byte #%11111010
+    .byte #%11111110
+    .byte #%00111100
+    .byte #%00011000
+    .byte #%00111100
+    .byte #%00111110
+    .byte #%01111111
+    .byte #%11111111
+    .byte #%11111010
+    .byte #%11111110
+    .byte #%00111100
+    .byte #%00011000
+    REPEAT 112
+    .byte #%00000000
+    REPEND
+
 Tiles
 Tile1
     .byte #%10000001
     .byte #%00000001 
-    .byte #%00000001
     .byte #%00000001
     .byte #%00000001
     .byte #%00000001
@@ -239,7 +359,6 @@ Tile2
     .byte #%11111111 ; Tile 2
 
 Tile3
-    .byte #%00000011 
     .byte #%00000111
     .byte #%00001111
     .byte #%00011111
@@ -262,13 +381,11 @@ Tile5
     .byte #%11111111
     .byte #%11111111
     .byte #%11111111
-    .byte #%11111111
     .byte #%11111111 ; Tile 5
 
 Tile6
     .byte #%00000000
     .byte #%00000000 
-    .byte #%00000000
     .byte #%00000000
     .byte #%00000000
     .byte #%00000000
